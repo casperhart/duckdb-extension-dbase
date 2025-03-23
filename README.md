c22496d741bf4a7f5480e8e862e2a56e26a8e6d0
Casper Hart <casperhart93@gmail.com>
2025-03-23 14:58:54 -0400

    initial working version

--- README.md
+++ README.md
@@ -1,93 +1,27 @@
-# DuckDB Rust extension template
-This is an **experimental** template for Rust based extensions based on the C Extension API of DuckDB. The goal is to 
-turn this eventually into a stable basis for pure-Rust DuckDB extensions that can be submitted to the Community extensions
-repository
+# Duckdb dbase extension
 
-Features:
-- No DuckDB build required
-- No C++ or C code required
-- CI/CD chain preconfigured
-- (Coming soon) Works with community extensions
+> [!WARNING]
+> This extension is not very well tested. Use at your own risk.
 
-## Cloning
+This extension is based on dbase-rs, and allows reading of dbf files using duckdb.
 
-Clone the repo with submodules
+Example:
 
-```shell
-git clone --recurse-submodules <repo>
 ```
-
-## Dependencies
-In principle, these extensions can be compiled with the Rust toolchain alone. However, this template relies on some additional
-tooling to make life a little easier and to be able to share CI/CD infrastructure with extension templates for other languages:
-
-- Python3
-- Python3-venv
-- [Make](https://www.gnu.org/software/make)
-- Git
-
-Installing these dependencies will vary per platform:
-- For Linux, these come generally pre-installed or are available through the distro-specific package manager.
-- For MacOS, [homebrew](https://formulae.brew.sh/).
-- For Windows, [chocolatey](https://community.chocolatey.org/).
-
-## Building
-After installing the dependencies, building is a two-step process. Firstly run:
-```shell
 make configure
-```
-This will ensure a Python venv is set up with DuckDB and DuckDB's test runner installed. Additionally, depending on configuration,
-DuckDB will be used to determine the correct platform for which you are compiling.
-
-Then, to build the extension run:
-```shell
 make debug
-```
-This delegates the build process to cargo, which will produce a shared library in `target/debug/<shared_lib_name>`. After this step, 
-a script is run to transform the shared library into a loadable extension by appending a binary footer. The resulting extension is written
-to the `build/debug` directory.
+duckdb -unsigned
 
-To create optimized release binaries, simply run `make release` instead.
-
-## Testing
-This extension uses the DuckDB Python client for testing. This should be automatically installed in the `make configure` step.
-The tests themselves are written in the SQLLogicTest format, just like most of DuckDB's tests. A sample test can be found in
-`test/sql/<extension_name>.test`. To run the tests using the *debug* build:
-
-```shell
-make test_debug
+D load  './build/debug/extension/read_dbf/read_dbf.duckdb_extension';
+D create view stations as select * from read_dbf('./test/data/stations.dbf');
+D select * from stations limit 1;
+┌─────────────────┬────────────┬────────────┬─────────┐
+│      name       │ marker-col │ marker-sym │  line   │
+│     varchar     │  varchar   │  varchar   │ varchar │
+├─────────────────┼────────────┼────────────┼─────────┤
+│ Van Dorn Street │ #0000ff    │ rail-metro │ blue    │
+└─────────────────┴────────────┴────────────┴─────────┘
 ```
 
-or for the *release* build:
-```shell
-make test_release
-```
-
-### Version switching 
-Testing with different DuckDB versions is really simple:
-
-First, run 
-```
-make clean_all
-```
-to ensure the previous `make configure` step is deleted.
-
-Then, run 
-```
-DUCKDB_TEST_VERSION=v1.1.2 make configure
-```
-to select a different duckdb version to test with
-
-Finally, build and test with 
-```
-make debug
-make test_debug
-```
-
-### Known issues
-This is a bit of a footgun, but the extensions produced by this template may (or may not) be broken on windows on python3.11 
-with the following error on extension load:
-```shell
-IO Error: Extension '<name>.duckdb_extension' could not be loaded: The specified module could not be found
-```
-This was resolved by using python 3.12
+To do:
+- [ ] prevent panic when referenced .dbf file does not exist.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+extern crate dbase;
 extern crate duckdb;
 extern crate duckdb_loadable_macros;
 extern crate libduckdb_sys;
 
+use dbase::{BufReadWriteFile, FieldIndex, FieldInfo, FieldType, FieldValue, File as DbaseFile};
 use duckdb::{
     core::{DataChunkHandle, Inserter, LogicalTypeHandle, LogicalTypeId},
     vtab::{BindInfo, InitInfo, TableFunctionInfo, VTab},
@@ -9,52 +11,188 @@ use duckdb::{
 };
 use duckdb_loadable_macros::duckdb_entrypoint_c_api;
 use libduckdb_sys as ffi;
+use std::sync::{Arc, Mutex};
 use std::{
     error::Error,
-    ffi::CString,
-    sync::atomic::{AtomicBool, Ordering},
+    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
 };
 
+// read only once created
 #[repr(C)]
-struct HelloBindData {
-    name: String,
+struct DbaseBindData {
+    filepath: String,
+    fields: Vec<FieldInfo>,
+    total_rows: usize,
 }
 
 #[repr(C)]
-struct HelloInitData {
+struct DbaseInitData {
+    current_row: AtomicUsize,
     done: AtomicBool,
+    file_handle: Arc<Mutex<DbaseFile<BufReadWriteFile>>>,
+    projection: Vec<usize>,
 }
 
-struct HelloVTab;
+struct DbaseVTab;
 
-impl VTab for HelloVTab {
-    type InitData = HelloInitData;
-    type BindData = HelloBindData;
+impl VTab for DbaseVTab {
+    type InitData = DbaseInitData;
+    type BindData = DbaseBindData;
 
     fn bind(bind: &BindInfo) -> Result<Self::BindData, Box<dyn std::error::Error>> {
-        bind.add_result_column("column0", LogicalTypeHandle::from(LogicalTypeId::Varchar));
-        let name = bind.get_parameter(0).to_string();
-        Ok(HelloBindData { name })
-    }
+        let filepath = bind.get_parameter(0).to_string();
+        let file = DbaseFile::open_read_only(&filepath).expect(
+            format!(
+                "Could not find file {:?} or corresponding memo file",
+                &filepath
+            )
+            .as_str(),
+        );
 
-    fn init(_: &InitInfo) -> Result<Self::InitData, Box<dyn std::error::Error>> {
-        Ok(HelloInitData {
-            done: AtomicBool::new(false),
+        let fields = file.fields();
+
+        for field in file.fields() {
+            let logical_type = match &field.field_type() {
+                FieldType::Character => LogicalTypeId::Varchar,
+                FieldType::Currency => LogicalTypeId::Float,
+                FieldType::Date => LogicalTypeId::Date,
+                FieldType::DateTime => LogicalTypeId::Timestamp,
+                FieldType::Double => LogicalTypeId::Double,
+                FieldType::Float => LogicalTypeId::Float,
+                FieldType::Integer => LogicalTypeId::Integer,
+                FieldType::Logical => LogicalTypeId::Boolean,
+                FieldType::Memo => LogicalTypeId::Varchar,
+                FieldType::Numeric => LogicalTypeId::Double,
+            };
+            bind.add_result_column(
+                &field.name().to_lowercase(),
+                LogicalTypeHandle::from(logical_type),
+            );
+        }
+
+        let total_rows = file.num_records();
+
+        Ok(DbaseBindData {
+            filepath,
+            fields: fields.to_vec(),
+            total_rows,
         })
     }
 
-    fn func(func: &TableFunctionInfo<Self>, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
+    fn init(info: &InitInfo) -> Result<Self::InitData, Box<dyn std::error::Error>> {
+        let bind_data = info.get_bind_data::<DbaseBindData>();
+        let filepath = unsafe { &(*bind_data).filepath };
+        let projection = info
+            .get_column_indices()
+            .into_iter()
+            .map(|x| x as usize)
+            .collect();
+
+        let file = DbaseFile::open_read_only(&filepath)?;
+
+        Ok(DbaseInitData {
+            current_row: AtomicUsize::new(0),
+            done: AtomicBool::new(false),
+            file_handle: Arc::new(Mutex::new(file)),
+            projection,
+        })
+    }
+
+    fn func(
+        func: &TableFunctionInfo<Self>,
+        output: &mut DataChunkHandle,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let init_data = func.get_init_data();
         let bind_data = func.get_bind_data();
-        if init_data.done.swap(true, Ordering::Relaxed) {
+
+        let mut dbase_file = init_data
+            .file_handle
+            .lock()
+            .expect("Unable to lock file handle");
+
+        let start_row = init_data
+            .current_row
+            .load(std::sync::atomic::Ordering::Relaxed);
+        if start_row >= bind_data.total_rows || init_data.done.load(Ordering::Relaxed) {
             output.set_len(0);
-        } else {
-            let vector = output.flat_vector(0);
-            let result = CString::new(format!("Rusty Quack {} 🐥", bind_data.name))?;
-            vector.insert(0, result);
-            output.set_len(1);
+            init_data.done.store(true, Ordering::Relaxed);
+            return Ok(());
         }
+
+        let batch_size = 5.min(bind_data.total_rows - start_row);
+
+        let mut output_row = 0;
+
+        for input_row in 0..batch_size {
+            if let Some(mut record) = dbase_file.record(input_row + start_row) {
+                if record.is_deleted()? {
+                    continue;
+                }
+                // there is no projection (e.g. select count(1))
+                if init_data.projection == [usize::MAX] {
+                    output_row += 1;
+                    continue;
+                }
+                for (col_idx, proj) in init_data.projection.iter().enumerate() {
+                    let mut vector = output.flat_vector(col_idx);
+                    let r = record
+                        .read_field(FieldIndex(*proj))
+                        .expect("unable to read field");
+
+                    match r {
+                        FieldValue::Character(Some(c)) => vector.insert(output_row, c.as_str()),
+                        FieldValue::Currency(c) => {
+                            let slice = vector.as_mut_slice::<f64>();
+                            slice[output_row] = c;
+                        }
+                        FieldValue::Date(Some(d)) => {
+                            let slice = vector.as_mut_slice::<i32>();
+                            slice[output_row] = d.to_unix_days();
+                        }
+                        FieldValue::Double(d) => {
+                            let slice = vector.as_mut_slice::<f64>();
+                            slice[output_row] = d
+                        }
+                        FieldValue::Numeric(Some(d)) => {
+                            let slice = vector.as_mut_slice::<f64>();
+                            slice[output_row] = d
+                        }
+                        FieldValue::DateTime(dt) => {
+                            let slice = vector.as_mut_slice::<i64>();
+                            slice[output_row] = dt.to_unix_timestamp();
+                        }
+                        FieldValue::Float(Some(f)) => {
+                            let slice = vector.as_mut_slice::<f32>();
+                            slice[output_row] = f;
+                        }
+                        FieldValue::Integer(i) => {
+                            let slice = vector.as_mut_slice::<i32>();
+                            slice[output_row] = i;
+                        }
+                        FieldValue::Logical(Some(b)) => {
+                            let slice = vector.as_mut_slice::<bool>();
+                            slice[output_row] = b;
+                        }
+                        FieldValue::Memo(m) => vector.insert(output_row, m.as_str()),
+                        FieldValue::Character(None)
+                        | FieldValue::Numeric(None)
+                        | FieldValue::Logical(None)
+                        | FieldValue::Date(None)
+                        | FieldValue::Float(None) => vector.set_null(output_row),
+                    }
+                }
+                output_row += 1;
+            };
+        }
+        init_data
+            .current_row
+            .fetch_add(batch_size, Ordering::Relaxed);
+        output.set_len(batch_size);
+
         Ok(())
+    }
+    fn supports_pushdown() -> bool {
+        true
     }
 
     fn parameters() -> Option<Vec<LogicalTypeHandle>> {
@@ -66,7 +204,7 @@ const EXTENSION_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[duckdb_entrypoint_c_api()]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>(EXTENSION_NAME)
+    con.register_table_function::<DbaseVTab>(EXTENSION_NAME)
         .expect("Failed to register hello table function");
     Ok(())
 }
